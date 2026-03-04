8bd3b740952194515bbb6da7a9541c1ef3919810
Nadil-Dulran <it23161788@my.sliit.lk>
2026-03-05 00:35:34 +0530

    Update controller authentication with phone

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,13 +4,22 @@ import jwt from 'jsonwebtoken';
 import pool from '../config/db';
 
 export const register = async (req: Request, res: Response) => {
-  const { name, email, password } = req.body;
+  const { name, email, phone, password } = req.body;
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
+  const [existing]: any = await pool.query(
+  'SELECT id FROM users WHERE email = ?',
+  [email]
+  );
+
+  if (existing.length > 0) {
+   return res.status(400).json({ message: 'Email already exists' });
+  }
+
   await pool.query(
-    'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
-    [name, email, hashedPassword]
+    'INSERT INTO users (name, email, phone, password) VALUES (?, ?, ?, ?)',
+    [name, email, phone || null, hashedPassword]
   );
 
   res.status(201).json({ message: 'User created successfully' });
@@ -42,5 +51,13 @@ export const login = async (req: Request, res: Response) => {
     { expiresIn: '1d' }
   );
 
-  res.json({ token });
+  res.json({
+  token,
+  user: {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    phone: user.phone,
+  },
+  });  
 };
