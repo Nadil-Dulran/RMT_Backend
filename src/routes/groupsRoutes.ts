d9c93a23e7306cf0a05260c20679d677773e705c
Nadil-Dulran <it23161788@my.sliit.lk>
2026-03-12 12:22:01 +0530

    Update group routes with members

--- src/routes/groupsRoutes.ts
+++ src/routes/groupsRoutes.ts
@@ -5,7 +5,10 @@ import {
   getGroups,
   getGroupById,
   updateGroup,
-  deleteGroup
+  deleteGroup,
+  addMember,
+  getMembers,
+  removeMember
 } from '../controllers/groupsController';
 
 const router = Router();
@@ -15,5 +18,10 @@ router.get('/', authenticateToken, getGroups);
 router.get('/:id', authenticateToken, getGroupById);
 router.patch('/:id', authenticateToken, updateGroup);
 router.delete('/:id', authenticateToken, deleteGroup);
+
+router.post('/:id/members', authenticateToken, addMember);
+router.get('/:id/members', authenticateToken, getMembers);
+router.delete('/:id/members/:userId', authenticateToken, removeMember);
+
 
 export default router;
