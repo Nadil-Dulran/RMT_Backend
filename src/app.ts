66e93b245a99b2dcc78b02b33167b1b65437a2a1
Nadil-Dulran <it23161788@my.sliit.lk>
2026-03-17 18:59:28 +0530

    Update routes of app

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import authRoutes from './routes/authRoutes';
 import userRoutes from './routes/userRoutes';
 import profileRoutes from './routes/profileRoutes';
 import groupsRoutes from './routes/groupsRoutes';
+import expensesRoutes from './routes/expensesRoutes';
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/profile', profileRoutes);
 app.use('/api/groups', groupsRoutes);
+app.use('/api/expenses', expensesRoutes);
 
 
 
