f84461e2a7b157ddc8c45e18ac0b48d552f50072
Nadil-Dulran <it23161788@my.sliit.lk>
2026-05-07 16:58:45 +0530

    Updates delete account routes

--- src/routes/profileRoutes.ts
+++ src/routes/profileRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { authenticateToken } from '../middleware/authMiddleware';
-import { getProfile, updateProfile } from '../controllers/profileController';
+import { deleteAccount, getProfile, updateProfile } from '../controllers/profileController';
 
 const router = Router();
 
 router.get('/', authenticateToken, getProfile);
 router.patch('/', authenticateToken, updateProfile);
+router.delete('/', authenticateToken, deleteAccount);
 
 export default router;
